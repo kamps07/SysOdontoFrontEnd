01bf9a38d7c1161f8d17ed87d7890c7f85602a44
João Vitor <joao007darki@gmail.com>
2024-04-03 22:21:04 -0300

    Atualização da Tela de Login

--- sysodonto/src/pages/Login/Login.jsx
+++ sysodonto/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css'; // Importe o arquivo CSS onde você irá definir os estilos
+import './Login.css';
+import TextSysOdonto from "../../Assets/TextSysOdonto.svg"
 
 export default function Login() {
 
@@ -9,21 +9,31 @@ export default function Login() {
     const [senha, setSenha] = useState("");
 
     return (
-        <div className="login-container">
-            <input
-                className="login-input"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder='E-mail' />
+        <>
+            <div className="login-wallpaper">
+                <div className='container'>
 
-            <input
-                className="login-input"
-                value={senha}
-                onChange={(e) => setSenha(e.target.value)}
-                placeholder='Senha'
-                type='password' />
+                    <div className="input-container">
+                        <img src={TextSysOdonto} />
+                        <input
+                            className="login-input"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder='E-mail' />
+                        <input
+                            className="login-input"
+                            value={senha}
+                            onChange={(e) => setSenha(e.target.value)}
+                            placeholder='Senha'
+                            type='password' />
+                        <div>
+                            <button className="login-button">Entrar</button>
+                        </div>
+                    </div>
+                </div>
 
-            <button onClick={Login}>Login</button>
-        </div>
+            </div>
+        </>
     );
 }
+
