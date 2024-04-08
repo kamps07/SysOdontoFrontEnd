6aa434ee7689dae1d0d7647548e5e25be4d734fb
luizagleao <luizagabriella.leaosilva@gmail.com>
2024-04-07 21:44:50 -0300

    Atualização da tela Login

--- sysodonto/src/pages/Login/Login.jsx
+++ sysodonto/src/pages/Login/Login.jsx
@@ -1,42 +1,53 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import TextSysOdonto from "../../Assets/TextSysOdonto.svg";
 import './Login.css';
-import TextSysOdonto from "../../Assets/TextSysOdonto.svg"
-import Odontograma from '../../Components/Odontograma/Odontograma';
 
 export default function Login() {
-
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
+    const [mostrarSenha, setMostrarSenha] = useState(false);
+
+    const toggleMostrarSenha = () => {
+        setMostrarSenha(!mostrarSenha);
+    };
 
     return (
-
         <>
-            <Odontograma></Odontograma>
-            {/* <div className="login-wallpaper">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <div className="login-wallpaper">
                 <div className='container'>
-
                     <div className="input-container">
-                        <img src={TextSysOdonto} />
+                        <div className='Logo-alinhamento'>
+                            <img src={TextSysOdonto} alt="Logo" />
+                        </div>
+                        <p className='Texto-login'>Faça login na sua conta</p>
                         <input
                             className="login-input"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            placeholder='E-mail' />
+                            placeholder='Digite seu E-mail' />
                         <input
                             className="login-input"
                             value={senha}
                             onChange={(e) => setSenha(e.target.value)}
-                            placeholder='Senha'
-                            type='password' />
-                        <div>
-                            <button className="login-button">Entrar</button>
+                            placeholder='Digite sua Senha'
+                            type={mostrarSenha ? 'text' : 'password'} />
+                        <div className='alinhamento'>
+                            <a
+                                className='destaque'
+                                onClick={toggleMostrarSenha}
+                            >
+                                {mostrarSenha ? 'Ocultar Senha' : 'Mostrar Senha'}
+                            </a>
                         </div>
+
+                        <button className="login-button">Entrar</button>
+
+                        <p> Não tem uma conta SysOdonto? <span className='destaque'>Cadastre-se agora</span></p>
                     </div>
                 </div>
-
-            </div> */}
+            </div>
         </>
     );
 }
-
