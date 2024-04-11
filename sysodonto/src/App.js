63bf00daf40eda8f06d61a64112635b2340f1e26
João Vitor <joao007darki@gmail.com>
2024-04-10 21:19:29 -0300

    Apagar Arquivo Route

--- sysodonto/src/App.js
+++ sysodonto/src/App.js
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom"
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro'
-import Routes from './Services/Routes';
 
 function App() {
   const router = createBrowserRouter([
