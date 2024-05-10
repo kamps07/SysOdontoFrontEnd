f1f6b1c315c9aa48f055dde235c19d6bc6146c44
kamps07 <kaue.kk10@gmail.com>
2024-05-09 22:18:33 -0300

    Adicionando função para navegar para Login na Home

--- sys-odonto/src/pages/Home/Home.jsx
+++ sys-odonto/src/pages/Home/Home.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header'
 import Pacientes from '../Pacientes/Pacientes';
 import styles from './Home.module.css'
 import Financeiro from '../Financeiro/Financeiro';
 import Agenda from '../Agenda/Agenda';
+import AuthService from '../../services/AuthService';
 
 export default function Home() {
+
+
+    const navigate = useNavigate();
+
+    function VerificarLogin() {
+        const usuarioEstaLogado = AuthService.VerificarSeUsuarioEstaLogado();
+    
+        if (!usuarioEstaLogado) {
+            navigate("/Login");
+        }
+      };
+    
+    
+      useEffect(() => {
+        VerificarLogin();
+    }, []);
 
     const [paginaSelecionada, setPaginaSelecionada] = useState("Agenda");
 
